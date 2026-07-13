e3de7b2d228f78dd73eb73398d847f61a91cca9b
krishnakumar-31 <krishnasgk003@gmail.com>
2026-07-13 20:41:16 +0530

    Production configuration

--- backend/app.py
+++ backend/app.py
@@ -66,4 +66,8 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+<<<<<<< HEAD
     app.run(host="0.0.0.0", port=port)
+=======
+    app.run(host="0.0.0.0", port=port)
+>>>>>>> 94e63ad (Production configuration)
