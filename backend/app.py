066baea047a7d0ad2c716d2aa27b6a4674e5595e
krishnakumar-31 <krishnasgk003@gmail.com>
2026-07-18 16:29:59 +0530

    Fix app.py

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,7 @@ CORS(app)
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+
 @app.route("/")
 def home():
     return jsonify({
@@ -17,57 +18,76 @@ def home():
         "endpoint": "/generate"
     })
 
+
 @app.route("/generate", methods=["POST"])
 def generate():
 
+    # Check if image is present
     if "image" not in request.files:
-        return jsonify({"error": "No image uploaded"}), 400
+        return jsonify({
+            "status": "error",
+            "message": "No image uploaded"
+        }), 400
 
     file = request.files["image"]
 
+    # Check filename
     if file.filename == "":
-        return jsonify({"error": "No image selected"}), 400
+        return jsonify({
+            "status": "error",
+            "message": "No image selected"
+        }), 400
 
+    # Validate extension
     allowed_extensions = {"png", "jpg", "jpeg"}
+
+    if "." not in file.filename:
+        return jsonify({
+            "status": "error",
+            "message": "Invalid file"
+        }), 400
 
     extension = file.filename.rsplit(".", 1)[1].lower()
 
     if extension not in allowed_extensions:
-        return jsonify({"error": "Only JPG, JPEG and PNG are allowed"}), 400
+        return jsonify({
+            "status": "error",
+            "message": "Only PNG, JPG and JPEG files are allowed"
+        }), 400
 
     filepath = os.path.join(UPLOAD_FOLDER, file.filename)
 
-    file.save(filepath)
-
     try:
+        # Save uploaded image
+        file.save(filepath)
 
+        # Generate caption
         caption = generate_caption(filepath)
 
-        os.remove(filepath)
-
-        return jsonify({
-    "status": "success",
-    "caption": caption,
-    "filename": file.filename,
-    "model": "BLIP",
-    "message": "Caption generated successfully"
-    })
-
-    except Exception as e:
-
+        # Delete uploaded image
         if os.path.exists(filepath):
             os.remove(filepath)
 
         return jsonify({
-            "error": str(e)
+            "status": "success",
+            "caption": caption,
+            "filename": file.filename,
+            "model": "BLIP",
+            "message": "Caption generated successfully"
+        })
+
+    except Exception as e:
+
+        # Delete uploaded image if an error occurs
+        if os.path.exists(filepath):
+            os.remove(filepath)
+
+        return jsonify({
+            "status": "error",
+            "message": str(e)
         }), 500
 
-import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-<<<<<<< HEAD
     app.run(host="0.0.0.0", port=port)
-=======
-    app.run(host="0.0.0.0", port=port)
->>>>>>> 94e63ad (Production configuration)
